60aedbe5d7da08ef7ab4863765964f925d8c9ba4
Yaser Bahrami <yaser.bahrami@gmail.com>
2021-10-28 23:55:18 +0330

    Add link book shopping

--- resources/views/panelUser/home.blade.php
+++ resources/views/panelUser/home.blade.php
@@ -3,9 +3,10 @@
     <div class="col-xs-12 col-md-4 col-lg-4 col-xl-4">
         @include('panelUser.boxProfile')
         @if(! is_null($bookoff))
-            <div class="alert alert-warning">
+            <div class="alert alert-warning text-center">
                 کد تخفیف خرید کتاب 'کوچینگ چیست':
                 <b>FARABOOK</b>
+                <a href="https://faracoach.com/product/%DA%A9%D8%AA%D8%A7%D8%A8-%DA%A9%D9%88%DA%86%DB%8C%D9%86%DA%AF/" class="btn btn-success mt-3" target="_blank">لینک خرید</a>
             </div>
         @endif
     </div>
