277067c4642f5729428e974b726a86529ea8bb1f
Yaser Bahrami <yaser.bahrami@gmail.com>
2021-11-03 12:42:36 +0330

    Add Counter

--- resources/views/panelAdmin/event_Isfahan.blade.php
+++ resources/views/panelAdmin/event_Isfahan.blade.php
@@ -6,12 +6,14 @@
                 {{ "تعداد ".$users->count()." نفر "}}
                 <table class="table table-striped">
                     <tr>
+                        <th>ردیف</th>
                         <th>نام</th>
                         <th>نام خانوادگی</th>
                         <th>تلفن</th>
                     </tr>
                     @foreach($users as $item)
                         <tr>
+                            <td>{{$loop->iteration }}</td>
                             <td>{{$item->fname}}</td>
                             <td>{{$item->lname}}</td>
                             <td>{{$item->tel}}</td>
