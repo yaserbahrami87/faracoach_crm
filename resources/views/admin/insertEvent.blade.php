7f6549eb7b0e3165817692bc409ad6b743d0aecc
Yaser Bahrami <yaser.bahrami@gmail.com>
2021-12-07 16:20:07 +0330

    Fix bug

--- resources/views/admin/insertEvent.blade.php
+++ resources/views/admin/insertEvent.blade.php
@@ -45,7 +45,7 @@
                         </div>
                         <div class="form-group">
                             <label for="fi">قیمت (تومان):<span class="text-danger">*</span></label>
-                            <input type="number" class="form-control" id="fi" name="fi" value="{{old('fi',$event->fi)}}"  />
+                            <input type="number" class="form-control" id="fi" name="fi" value="{{old('fi')}}"  />
                             <small class="text-muted">در صورت رایگان بودن عدد 0 را وارد و یا هیچ عددی وارد نکنید</small>
                         </div>
                         <div class="form-row">
