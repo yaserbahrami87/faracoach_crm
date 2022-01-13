d65566bc85bb3f3a552ccd76b6aaa33a8696ebed
Yaser Bahrami <yaser.bahrami@gmail.com>
2022-01-13 12:31:23 +0330

    Link tel Students to folowups

--- resources/views/admin/courseStudents.blade.php
+++ resources/views/admin/courseStudents.blade.php
@@ -6,7 +6,9 @@
             <tr>
                 <th></th>
                 <th class="text-center">نام و نام وخانوادگی</th>
+                <th class="text-center">شماره همراه</th>
                 <th class="text-center">تاریخ ثبت نام</th>
+
                 <th class="text-center">کدرهگیری</th>
             </tr>
 
@@ -20,12 +22,20 @@
                             <img src="{{asset('/documents/users/'.$item->user->personal_image)}}"  width="50px" height="50px" class="rounded-circle "/>
                         @endif
                     </td>
+
                     <td class="text-center">
                         {{$item->user->fname." ".$item->user->lname}}
                     </td>
+                    <td class="text-center" dir="ltr">
+                        <a href="/admin/user/{{$item->user->id}}" target="_blank">
+                        {{$item->user->tel}}
+                        </a>
+                    </td>
+
                     <td class="text-center">
                         {{$item->date_fa}}
                     </td>
+
 
                     <td class="text-center">
                         @foreach($item->user->checkouts->where('status','=',1)->where('product_id','=',$item->course_id)->where('type','=','course') as $item2)
