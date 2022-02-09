f73feee66bf5569caee82dacf3472609aca84abd
Yaser Bahrami <yaser.bahrami@gmail.com>
2022-02-09 15:38:50 +0330

    Fix bug

--- resources/views/landings/jashn_options.blade.php
+++ resources/views/landings/jashn_options.blade.php
@@ -25,12 +25,10 @@
                     <tr>
                         <td> {{$landPage['options'][1]}}</td>
                         <td>
-                            @if($landPage['resultoptions'][1])
-                                <select class="custom-select" name="resultoptions[]">
-                                    <option value="0" selected >انجام نشده</option>
-                                    <option @if($landPage['resultoptions'][1]==1) selected  @endif value="1">انجام شده</option>
-                                </select>
-                            @endif
+                            <select class="custom-select" name="resultoptions[]">
+                                <option value="0" selected >انجام نشده</option>
+                                <option @if($landPage['resultoptions'][1]==1) selected  @endif value="1">انجام شده</option>
+                            </select>
                         </td>
                     </tr>
                 @endif
