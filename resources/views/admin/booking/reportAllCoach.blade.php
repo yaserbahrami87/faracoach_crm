133413c03169b9e8ea016e0701e95d02da72ea92
Yaser Bahrami <yaser.bahrami@gmail.com>
2022-02-13 01:53:00 +0330

    Fix bug

--- resources/views/admin/booking/reportAllCoach.blade.php
+++ resources/views/admin/booking/reportAllCoach.blade.php
@@ -44,7 +44,12 @@
                     @foreach($reserveBooking as $item)
                         <tr>
                             <th>{{$loop->iteration}}</th>
-                            <td>{{$item->coach->user->fname.' '.$item->coach->user->lname}}</td>
+                            <td>
+                                <a href="/admin/booking/{{$item->coach->user->id}}/report" target="_blank">
+                                    <img src="{{asset('/documents/users/'.$item->coach->user->personal_image)}}" width="50px" height="50px" class="rounded-circle" />
+                                    {{$item->coach->user->fname.' '.$item->coach->user->lname}}
+                                </a>
+                            </td>
                             <td>{{$item->start_date}}</td>
                             <td>{{$item->start_time}}</td>
                             <td dir="ltr">
@@ -107,7 +112,12 @@
                     @foreach($cancelBooking as $item)
                         <tr>
                             <th>{{$loop->iteration}}</th>
-                            <td>{{$item->coach->user->fname.' '.$item->coach->user->lname}}</td>
+                            <td>
+                                <a href="/admin/booking/{{$item->coach->user->id}}/report" target="_blank">
+                                    <img src="{{asset('/documents/users/'.$item->coach->user->personal_image)}}" width="50px" height="50px" class="rounded-circle" />
+                                    {{$item->coach->user->fname.' '.$item->coach->user->lname}}
+                                </a>
+                            </td>
                             <td>{{$item->start_date}}</td>
                             <td>{{$item->start_time}}</td>
                             <td dir="ltr">
