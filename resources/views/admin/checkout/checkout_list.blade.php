46ebff4df6f5ceec5878951704af2f2cba62ed2a
unknown <yaser.bahrami@gmail.com>
2022-04-19 12:44:12 +0430

    Add checkout reserve

--- resources/views/admin/checkout/checkout_list.blade.php
+++ resources/views/admin/checkout/checkout_list.blade.php
@@ -26,12 +26,15 @@
     <div class="col-12 table-responsive">
         <nav>
             <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                <a class="nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">لاگ درگاه <span class="badge badge-secondary">{{$checkout->count()}}</span></a>
+                <a class="nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">لاگ درگاه<span class="badge badge-secondary">{{$checkout->count()}}</span></a>
                 <a class="nav-link" id="nav-checkoutAccess-tab" data-toggle="tab" href="#nav-checkoutAccess" role="tab" aria-controls="nav-checkoutAccess" aria-selected="false">پرداخت شده <span class="badge badge-success">{{$checkoutAccess->count()}}</span></a>
+                <a class="nav-link" id="nav-checkoutCoach-tab" data-toggle="tab" href="#nav-checkoutCoach" role="tab" aria-controls="nav-checkoutCoach" aria-selected="false">پرداخت کلینیک <span class="badge badge-success">{{$checkoutAccess->where('type','=','reserve')->count()}}</span></a>
+
             </div>
         </nav>
         <div class="tab-content" id="nav-tabContent">
             <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                <p>جمع مبالغ: {{number_format($checkout->sum('price'))}} تومان</p>
                 <table class="dataTable table table-striped table-bordered" style="width:100%">
                     <thead>
                     <tr>
@@ -72,8 +75,9 @@
                                 </td>
                                 <td>{{number_format($item->price)}}</td>
                                 <td>{{$item->description}}</td>
-                                <td class="text-right">{{substr($item->dateTime,0,5)  }}</td>
+
                                 <td class="text-right">{{substr($item->dateTime,5)  }}</td>
+                                <td class="text-right">{{substr($item->dateTime,0,5)  }}</td>
                                 <td class="text-right">{{$item->authority}}</td>
                             </tr>
                         @endforeach
@@ -81,6 +85,7 @@
                 </table>
             </div>
             <div class="tab-pane fade" id="nav-checkoutAccess" role="tabpanel" aria-labelledby="nav-checkoutAccess-tab">
+                <p>جمع مبالغ: {{number_format($checkoutAccess->sum('price'))}} تومان</p>
                 <table class="dataTable table table-striped table-bordered" style="width:100%">
                     <thead>
                     <tr>
@@ -118,8 +123,61 @@
                             </td>
                             <td>{{number_format($item->price)}}</td>
                             <td>{{$item->description}}</td>
-                            <td class="text-right">{{substr($item->dateTime,0,5)  }}</td>
                             <td class="text-right">{{substr($item->dateTime,5)  }}</td>
+                            <td class="text-right">{{substr($item->dateTime,0,5)  }}</td>
+                            <td class="text-right">{{$item->authority}}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <!--***** پرداختی های جلسات رزرو ****** -->
+            <div class="tab-pane fade show active" id="nav-checkoutCoach" role="tabpanel" aria-labelledby="nav-checkoutCoach-tab">
+                <p>جمع مبالغ: {{number_format($checkoutAccess->where('type','=','reserve')->sum('price'))}} تومان</p>
+                <table class="dataTable table table-striped table-bordered" style="width:100%">
+                    <thead>
+                    <tr>
+                        <th>مشخصات</th>
+                        <th>محصول</th>
+                        <th>واریزی(تومان)</th>
+                        <th>توضیحات</th>
+                        <th>ساعت</th>
+                        <th>تاریخ</th>
+                        <th>کد</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($checkoutAccess->where('type','=','reserve') as $item)
+
+                        <tr class="@if($item->status==1) bg-success  @endif">
+                            <td>
+
+                                <a href="{{route('showUserForAdmin',$item->user->id)}}" target="_blank">
+                                    @if(is_null($item->user->fname)&&(is_null($item->user->lname)))
+                                        {{$item->user->tel}}
+                                    @else
+                                        {{$item->user->fname.' '.$item->user->lname}}
+                                    @endif
+                                </a>
+
+                            </td>
+                            <td>
+                                @if($item->type=='course')
+                                    {{$item->course->course}}
+                                @elseif($item->type=='event')
+                                    {{$item->event->event}}
+                                @elseif($item->type=='reserve')
+                                    جلسه کوچینگ {{$item->reserve['booking']['coach']['user']['fname'].' '.$item->reserve['booking']['coach']['user']['lname'].' - '.$item->reserve['booking']['start_date']}}
+                                @elseif($item->type=='ghest')
+                                    پرداخت قسط
+                                @endif
+                            </td>
+                            <td>{{number_format($item->price)}}</td>
+                            <td>{{$item->description}}</td>
+
+                            <td class="text-right">{{substr($item->dateTime,5)  }}</td>
+                            <td class="text-right">{{substr($item->dateTime,0,5)  }}</td>
                             <td class="text-right">{{$item->authority}}</td>
                         </tr>
                     @endforeach
