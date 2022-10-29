e27b49bd3d21d4b37dd3cc99f3734718d052000b
Yaser Bahrami <yaser.bahrami@gmail.com>
2022-10-29 11:44:40 +0330

    add show sum price scholarship

--- app/Http/Controllers/ScholarshipController.php
+++ app/Http/Controllers/ScholarshipController.php
@@ -557,10 +557,8 @@ class ScholarshipController extends BaseController
                     {
                         //حضوری ها در مصاحبه مقدار 1 دارند در جدول درس 2
                         //آنلاین ها در مصاحبه مقدار 2 دارند در جدول درس 1
-
-                        $query->orwhere('type_course','=',1)
-                        //برای نمایش دوره های انلاین برای همه
-                        ->orwhere('type_course','=',2);
+                        $query->where('type_course','=',1)
+                                ->orwhere('type_course','=',2);
                     })
                     ->orderby('id','desc')
                     ->get();
