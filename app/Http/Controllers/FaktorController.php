81191382ad6389ee1c4fa784ea201f894be50555
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-06-18 12:32:55 +0430

    fix bug faktor

--- app/Http/Controllers/FaktorController.php
+++ app/Http/Controllers/FaktorController.php
@@ -67,8 +67,11 @@ class FaktorController extends BaseController
 
         if($faktor)
         {
-            $msg=$faktor->user->fname.' '.$faktor->user->lname. " عزیز\nمبلغ ".$request->fi." تومان ".$request->description." از حساب تهاتر شما کسر گردید\nفراکوچ ";
-            $this->sendSms($faktor->user->tel,$msg);
+            if($request->status==2)
+            {
+                $msg=$faktor->user->fname.' '.$faktor->user->lname. " عزیز\nمبلغ ".$request->fi." تومان ".$request->description." از حساب تهاتر شما کسر گردید\nفراکوچ ";
+                $this->sendSms($faktor->user->tel,$msg);
+            }
             alert()->success('فاکتور با موفقیت ایجاد شد')->persistent('بستن');
         }
         else
