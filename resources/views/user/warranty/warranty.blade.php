f6d1d8b279de0c4a8cbdcfe2006d6366074ff645
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-07-22 12:44:29 +0430

    fix bug

--- resources/views/user/warranty/warranty.blade.php
+++ resources/views/user/warranty/warranty.blade.php
@@ -164,7 +164,7 @@
                 @endif
 
                 @if(Auth::user()->checkouts->where('status','=',1)->where('product_id','=',$course->id)->first()['type']=='invoice')
-                    @if(is_null(Auth::user()->checkouts->where('status','=',1)->where('product_id','=',$course->id)->first()->invoice['pre_payment']))
+                    @if(!is_null(Auth::user()->checkouts->where('status','=',1)->where('product_id','=',$course->id)->first()->invoice['pre_payment']))
                         {{number_format(Auth::user()->checkouts->where('status','=',1)->where('product_id','=',$course->id)->first()->invoice['pre_payment'])}}
                     @else
                         {{number_format(Auth::user()->checkouts->where('status','=',1)->where('product_id','=',$course->id)->first()->order['pre_paymant'])}}
