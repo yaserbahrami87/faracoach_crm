c68061f66bd7f15ebee9cc0171139320396a70a3
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-07-29 17:38:42 +0430

    add booking Profile admin

---
+++ resources/views/admin/user/booking.blade.php
@@ -0,0 +1,36 @@
+<div class="col-12 table-responsive mb-3">
+
+    <table class="dataTable table table-striped table-bordered" style="width:100%">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>شماره جلسه</th>
+            <th>کوچ</th>
+
+            <th>تاریخ جلسه </th>
+            <th>ساعت جلسه </th>
+
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($user->reserves as $item)
+            <tr>
+                <td>{{$loop->iteration}}</td>
+                <td>
+                    <a href="/admin/booking/{{$item->id}}/showadminbooking">{{$item->id}}</a>
+
+                </td>
+                <td>
+                    {{$item->booking->coach->user->fname.' '.$item->booking->coach->user->lname}}
+                </td>
+                <td>{{$item->booking->start_date}}</td>
+                <td>{{$item->booking->start_time}}</td>
+
+            </tr>
+        @endforeach
+        </tbody>
+
+    </table>
+</div>
+
+
