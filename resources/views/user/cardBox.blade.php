56c3daeb6107071cdadb3f21b14237f9fc445960
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-09-20 18:06:59 +0430

    fix bug

--- resources/views/user/cardBox.blade.php
+++ resources/views/user/cardBox.blade.php
@@ -7,7 +7,7 @@
     </style>
 @endsection
 
-@if($user->tel_verified==0)
+@if(Auth::user()->tel_verified==0)
     <!--
     <div class="col-12">
 
@@ -47,54 +47,6 @@
     </div>
 @endif
 
-<!--
-
-<div class="col-lg-3 col-md-6 col-sm-6">
-    <div class="info-box">
-        <span class="info-box-icon bg-info elevation-1"><i class="fas fa-envelope"></i></span>
-        <div class="info-box-content">
-            <span class="info-box-text">
-                <a href="/panel/messages/">خوانده نشده</a>
-            </span>
-            <span class="info-box-number">
-                  {{$unreadMessage}}
-                  <small>پیام</small>
-                </span>
-        </div>
-    </div>
-</div>
-
-<div class="col-lg-3 col-md-6 col-sm-6">
-    <div class="info-box">
-        <span class="info-box-icon bg-warning elevation-1 text-light"><i class="fas fa-project-diagram"></i></span>
-        <div class="info-box-content">
-            <span class="info-box-text">
-                <a href="/panel/introduced">افراد دعوت شده</a>
-            </span>
-            <span class="info-box-number">
-                  {{$countIntroducedUser}}
-                  <small>نفر</small>
-            </span>
-        </div>
-    </div>
-</div>
-<div class="col-lg-3 col-md-6 col-sm-6">
-    <div class="info-box">
-        <span class="info-box-icon bg-info elevation-1"><i class="fas fa-medal"></i></span>
-        <div class="info-box-content">
-            <span class="info-box-text">
-                <a href="#" data-toggle="modal" data-target="#ScoreModal">امتیازات</a>
-            </span>
-            <span class="info-box-number">
-                  {{$score}}
-                  <small>امتیاز</small>
-            </span>
-        </div>
-    </div>
-</div>
--->
-
-
 
 <div class="container-fluid  " >
     <div class="row pt-3" id="board">
@@ -111,30 +63,30 @@
             </a>
         </div>
 
-        @if($user->status_coach==0)
+        @if(Auth::user()->status_coach==0)
             <div class="col-lg-3 col-md-6 col-sm-6 text-center mt-1 items">
                 <a href="panel/coach/create"  class="btn btn-info shadow-lg">
                     <i class="bi bi-person-circle" ></i>
                     <p>همکاری به عنوان کوچ</p>
                 </a>
             </div>
-        @elseif($user->status_coach=='-1')
+        @elseif(Auth::user()->status_coach=='-1')
             <div class="col-lg-3 col-md-6 col-sm-6 text-center mt-1 items">
                 <a href="#"  class="btn btn-info shadow-lg">
                     <i class="bi bi-person-circle" ></i>
                     <p>درخواست همکاری شما در حال بررسی است</p>
                 </a>
             </div>
-        @elseif($user->status_coach==1)
+        @elseif(Auth::user()->status_coach==1)
             <div class="col-lg-3 col-md-6 col-sm-6   text-center mt-1 items ">
-                <a href="/panel/coach/{{$user->id_coaches_table}}/edit"  class="btn btn-info  shadow-lg">
+                <a href="/panel/coach/{{Auth::user()->id_coaches_table}}/edit"  class="btn btn-info  shadow-lg">
                     <i class="bi bi-person-circle" ></i>
                     <p>شما یک کوچ هستید</p>
                 </a>
             </div>
-        @elseif($user->status_coach==-2)
+        @elseif(Auth::user()->status_coach==-2)
             <div class="col-lg-3 col-md-6 col-sm-6  text-center mt-1 items">
-                <a href="/panel/coach/{{$user->id_coaches_table}}/edit"  class="btn btn-info shadow-lg">
+                <a href="/panel/coach/{{Auth::user()->id_coaches_table}}/edit"  class="btn btn-info shadow-lg">
                     <i class="bi bi-person-circle" ></i>
                     <p>درخواست همکاری شما رد شد</p>
                 </a>
@@ -179,9 +131,10 @@
 
 
             <div class="list-group">
-                @if($courses->count()>0)
-                    @foreach($courses as $item)
-                        <a href="#" class="list-group-item list-group-item-action" target="_blank">{{$item->course}}</a>
+                @if(Auth::user()->students->count()>0)
+
+                    @foreach(Auth::user()->students as $item)
+                        <a href="#" class="list-group-item list-group-item-action" target="_blank">{{$item->course->course}}</a>
                     @endforeach
                 @else
                     <div class="alert alert-warning">
@@ -196,70 +149,3 @@
 
     </div>
 </div>
-
-<!-- Modal Scores-->
-<div class="modal fade" id="ScoreModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">گزارش امتیازات</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <div class="row">
-              <div class="col-12 mr-3">
-                <ul class="list-group p-0 border-0">
-
-                    <li class="list-group-item">
-                        <div class="row border pt-2">
-                            <div class="col-md-6 col-2">
-                                <p> افراد دعوت شده {{$countIntroducedUser}} نفر</p>
-                            </div>
-                            <div class="col-md-6 col-3 text-right">
-                                <p>{{$scoreIntroducedUser}} امتیاز</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="row border pt-2">
-                            <div class="col-md-6 col-2">
-                                <p> ثبت شماره همراه</p>
-                            </div>
-                            <div class="col-md-6 col-3 text-right">
-                                <p>{{$scoreTelverify}} امتیاز</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="row border pt-2">
-                            <div class="col-md-6 col-2">
-                                <p> ثبت پست الکترونیکی</p>
-                            </div>
-                            <div class="col-md-6 col-3 text-right">
-                                <p>{{$scoreEmailverify}} امتیاز</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="row border pt-2">
-                            <div class="col-md-6 col-2">
-                                <p>دانشجوی معرفی شده {{$SuccessIntroduced}} نفر</p>
-                            </div>
-                            <div class="col-md-6 col-3 text-right">
-                                <p>{{$scoreSuccess}} امتیاز</p>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-
-            </div>
-          </div>
-        </div>
-        <div class="modal-footer">
-
-        </div>
-      </div>
-    </div>
-  </div>
