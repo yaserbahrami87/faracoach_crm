9ac63707f2e88eccec00a3a6a6590cb21019b6e5
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-09-21 10:47:42 +0430

    fix bug route

--- routes/web.php
+++ routes/web.php
@@ -259,8 +259,7 @@ Route::middleware('can:isAdmin')->prefix('admin')->group(function () {
     Route::patch('/profile/update/{user}','UserController@update');
     Route::patch('/user/{id}/changeType','UserController@changeType');
     Route::get('/user/{user}/login','UserController@loginWithUser');
-    Route::get('/users/all','UserController@
-    ');
+    Route::get('/users/all','UserController@showAll');
     Route::get('/users/all/category/','UserController@showCategoryAllUsers');
 
 
