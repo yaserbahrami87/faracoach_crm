cbf3a5654f0a978b77326c182e75138104f4d10f
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-10-04 16:21:20 +0330

    Auth Admin

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -129,8 +129,14 @@
                                             @endif
                                         </form>
 
-
-                                        @if( $user->type!=3 && $user->type!=4 && $user->type!=5 && $user->type!=6 && $user->type!=7)
+                                        @if(Auth::user()->type==2)
+                                            <form method="get" action="/admin/user/{{$user->id}}/login" onsubmit="return window.confirm('آیا میخواهید با اکانت کاربر وارد سایت شوید؟')">
+                                                {{csrf_field()}}
+                                                <div class="input-group mb-1 mt-1 border-bottom border-1 pb-1 ">
+                                                    <input type="submit"  class="btn btn-primary" value="ورود با اکانت کاربر"  />
+                                                </div>
+                                            </form>
+                                        @elseif($user->type!=2 && $user->type!=3 && $user->type!=4 && $user->type!=5 && $user->type!=6 && $user->type!=7)
                                             <form method="get" action="/admin/user/{{$user->id}}/login" onsubmit="return window.confirm('آیا میخواهید با اکانت کاربر وارد سایت شوید؟')">
                                                 {{csrf_field()}}
                                                 <div class="input-group mb-1 mt-1 border-bottom border-1 pb-1 ">
