ad0cf10c3d68d6ef165435c2975d1194516cb2eb
Yaser Bahrami <yaser.bahrami@gmail.com>
2024-05-05 12:35:52 +0430

    add migrations

---
+++ database/migrations/2024_04_28_154758_add_field_description.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldDescription extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('description',250)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+}
