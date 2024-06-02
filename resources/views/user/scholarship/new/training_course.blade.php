e2d0e6f51c3bb771ec92392c60de5a91c3c9bea9
Yaser Bahrami <yaser.bahrami@gmail.com>
2024-06-02 11:57:09 +0430

    change links video

--- resources/views/user/scholarship/new/training_course.blade.php
+++ resources/views/user/scholarship/new/training_course.blade.php
@@ -39,13 +39,12 @@
                     <h5 class="float-right">ویدئو آموزشی</h5>
                 </div>
                 <div class="ibox-content" style="margin-top: 10px">
-                    {{--                    <style>.h_iframe-aparat_embed_frame{position:relative;}.h_iframe-aparat_embed_frame .ratio{display:block;width:100%;height:auto;}.h_iframe-aparat_embed_frame iframe{position:absolute;top:0;left:0;width:100%;height:100%;}</style><div class="h_iframe-aparat_embed_frame"><span style="display: block;padding-top: 57%"></span><iframe src="https://www.aparat.com/video/video/embed/videohash/yCEac/vt/frame" allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe></div>--}}<div class="container">
                         <div class="portfolioContainer">
                             <div class="row">
                                 <div class="col-md-6 col-12 webdesign illustrator">
                                     <div class="gal-detail thumb">
                                         <video controls>
-                                            <source src="https://dll.faracoach.com/dl/webinar%20sch2024/part%201.mp4" />
+                                            <source src="/videos/webinar%20sch2024/part%201.mp4" />
                                         </video>
                                         <h6 class="text-center">فیلم اول</h6>
                                         <div class="ga-border"></div>
@@ -54,7 +53,7 @@
                                 <div class="col-md-6 col-12 webdesign illustrator">
                                     <div class="gal-detail thumb">
                                         <video controls>
-                                            <source src="https://dll.faracoach.com/dl/webinar%20sch2024/part%202.mp4" />
+                                            <source src="/videos/webinar%20sch2024/part%202.mp4" />
                                         </video>
                                         <h6 class="text-center">فیلم دوم</h6>
                                         <div class="ga-border"></div>
@@ -63,7 +62,7 @@
                                 <div class="col-md-6 col-12 webdesign illustrator">
                                     <div class="gal-detail thumb">
                                         <video controls>
-                                            <source src="http://dll.faracoach.com/dl/webinar%20sch2024/part%203.mp4" />
+                                            <source src="/videos/webinar%20sch2024/part%203.mp4" />
                                         </video>
                                         <h6 class="text-center">فیلم سوم</h6>
                                         <div class="ga-border"></div>
@@ -72,7 +71,7 @@
                                 <div class="col-md-6 col-12 webdesign illustrator">
                                     <div class="gal-detail thumb">
                                         <video controls>
-                                            <source src="http://dll.faracoach.com/dl/webinar%20sch2024/part%204.mp4" />
+                                            <source src="/videos/webinar%20sch2024/part%204.mp4" />
                                         </video>
                                         <h6 class="text-center">فیلم چهارم</h6>
                                         <div class="ga-border"></div>
@@ -81,7 +80,7 @@
                                 <div class="col-md-6 col-12 webdesign illustrator">
                                     <div class="gal-detail thumb">
                                         <video controls>
-                                            <source src="http://dll.faracoach.com/dl/webinar%20sch2024/part%205.mp4" />
+                                            <source src="/videos/webinar%20sch2024/part%205.mp4" />
                                         </video>
                                         <h6 class="text-center">فیلم پنجم</h6>
                                         <div class="ga-border"></div>
@@ -90,23 +89,25 @@
                                 <div class="col-md-6 col-12 webdesign illustrator">
                                     <div class="gal-detail thumb">
                                         <video controls>
-                                            <source src="https://dll.faracoach.com/dl/webinar%20sch2024/part%206.mp4" />
+                                            <source src="/videos/webinar%20sch2024/part%206.mp4" />
                                         </video>
                                         <h6 class="text-center">فیلم ششم</h6>
                                         <div class="ga-border"></div>
                                     </div>
-                                </div><div class="col-md-6 col-12 webdesign illustrator">
+                                </div>
+                                <div class="col-md-6 col-12 webdesign illustrator">
                                     <div class="gal-detail thumb">
                                         <video controls>
-                                            <source src="https://dll.faracoach.com/dl/webinar%20sch2024/part%207.mp4" />
+                                            <source src="/videos/webinar%20sch2024/part%207.mp4" />
                                         </video>
                                         <h6 class="text-center">فیلم هفتم</h6>
                                         <div class="ga-border"></div>
                                     </div>
-                                </div><div class="col-md-6 col-12 webdesign illustrator">
+                                </div>
+                                <div class="col-md-6 col-12 webdesign illustrator">
                                     <div class="gal-detail thumb">
                                         <video controls>
-                                            <source src="https://dll.faracoach.com/dl/webinar%20sch2024/part%208.mp4" />
+                                            <source src="/videos/webinar%20sch2024/part%208.mp4" />
                                         </video>
                                         <h6 class="text-center">فیلم هشتم</h6>
                                         <div class="ga-border"></div>
@@ -115,7 +116,7 @@
                                 <div class="col-md-6 col-12 webdesign illustrator">
                                     <div class="gal-detail thumb">
                                         <video controls>
-                                            <source src="https://dll.faracoach.com/dl/webinar%20sch2024/part%209.mp4" />
+                                            <source src="/videos/webinar%20sch2024/part%209.mp4" />
                                         </video>
                                         <h6 class="text-center">فیلم نهم</h6>
                                         <div class="ga-border"></div>
@@ -124,7 +125,7 @@
                                 <div class="col-md-6 col-12 webdesign illustrator">
                                     <div class="gal-detail thumb">
                                         <video controls>
-                                            <source src="http://dll.faracoach.com/dl/webinar%20sch2024/part%2010.mp4" />
+                                            <source src="/videos/webinar%20sch2024/part%2010.mp4" />
                                         </video>
                                         <h6 class="text-center">فیلم دهم</h6>
                                         <div class="ga-border"></div>
@@ -133,7 +134,7 @@
                                 <div class="col-md-6 col-12 webdesign illustrator">
                                     <div class="gal-detail thumb">
                                         <video controls>
-                                            <source src="http://dll.faracoach.com/dl/webinar%20sch2024/part%2011.mp4" />
+                                            <source src="/videos/webinar%20sch2024/part%2011.mp4" />
                                         </video>
                                         <h6 class="text-center">فیلم یازدهم</h6>
                                         <div class="ga-border"></div>
@@ -172,7 +173,8 @@
                         @endif
                 </div>
             </div>
+            </div>
         </div>
     </div>
 </div>
-</div>
+
