f28b6be09eda18c882d3699bebe821cd08cf7f7f
Yaser Bahrami <yaser.bahrami@gmail.com>
2024-06-02 13:34:35 +0430

    fandamental

--- resources/views/user/scholarship/new/fandamental.blade.php
+++ resources/views/user/scholarship/new/fandamental.blade.php
@@ -29,8 +29,11 @@
         <h5> دانشپذیر عزیز سلام <br> شما تا این مرحله در دوره مقدماتی آموزش کوچینگ شرکت و آزمون آن را با موفقیت پشت سر گذاشته اید . اکنون شما مجاز به شرکت در دوره 20 ساعته فاندامنتال آموزش کوچینگ هستید . <br>
         لطفا جهت ثبت نام و مشاهده توضیحات بیشتر به صفحه ثبت نام این دوره مراجعه نماید. </h5>
     </div>
-
-    <div class="col-12 col-md-12 mt-4 text-center" id="fundamental">
-        <button class="btn"> <a href="https://my.faracoach.com/product/fundamental">برای ثبت نام دوره فاندامنتال کلیک کنید  </a></button>
-    </div>
+    @if(Auth::user()->purchases->where('id',3)->first())
+        <div class="col-12 col-md-12 mt-4 text-center" id="fundamental">
+            <button class="btn"> <a href="https://my.faracoach.com/product/fundamental">برای ثبت نام دوره فاندامنتال کلیک کنید  </a></button>
+        </div>
+    @else
+        <div class="col-12 col-md-6 mx-auto mx-auto alert alert-success text-center" >شما با موفقیت دوره را خریداری کردید</div>
+    @endif
 </div>
