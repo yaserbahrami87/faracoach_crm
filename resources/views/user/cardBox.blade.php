41abcab4722ef934062e8aa46c9d0f27f37211db
Yaser Bahrami <yaser.bahrami@gmail.com>
2024-06-09 16:31:07 +0430

    add exam

--- resources/views/user/cardBox.blade.php
+++ resources/views/user/cardBox.blade.php
@@ -108,7 +108,11 @@
                 <i class="bi bi-camera-reels-fill" ></i>
                 <p> ویدئوی وبینار تمامیت</p>
             </a>
+
         </div>
+
+
+
         <!--
                <div class="col-lg-3 col-md-6 col-sm-6 text-center mt-1 items">
                    <a href="/panel/scientific_support/create"  class="btn btn-success">
@@ -123,14 +127,15 @@
                    </a>
                </div>
 
-               <div class="col-lg-3 col-md-6 col-sm-6 text-center mt-1 items">
-                   <a href="/panel/effectiveListenings/create" class="btn btn-success">
-                       <i class="bi bi-ear-fill"></i>
-                       <p>ارزیابی گوش دادن موثر</p>
-                   </a>
-               </div>
+
 
                -->
+        <div class="col-lg-3 col-md-6 col-sm-6 text-center mt-1 items">
+            <a href="/panel/effectiveListenings/create" class="btn btn-success">
+                <i class="bi bi-ear-fill"></i>
+                <p>ارزیابی گوش دادن موثر</p>
+            </a>
+        </div>
 
         <div class="col-12 mt-2 border-bottom border-1">
             <h5>آخرین دوره های ثبت نام شده</h5>
