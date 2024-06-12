d71bc3f42232ecb8db6a64fb117bd1dc2c92da7c
Yaser Bahrami <yaser.bahrami@gmail.com>
2024-06-12 12:51:36 +0430

    add comment courses

--- app/comment.php
+++ app/comment.php
@@ -9,4 +9,9 @@ class comment extends Model
     protected $fillable = [
         'user_id','post_id' ,'comment','status','type','date_fa','time_fa'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User','user_id','id');
+    }
 }
